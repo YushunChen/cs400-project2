3038987b943e1dc9d94e1569e8c1e53474b46923
Patrick <patches72790@gmail.com>
2020-10-18 16:27:28 -0500

    updated regex format for months/days

--- data/Birthday.java
+++ data/Birthday.java
@@ -15,14 +15,12 @@ import java.util.regex.PatternSyntaxException;
 import java.util.regex.Matcher;
 
 /**
- * This class represents a Birthday object to be contained within the Red
- * Black Tree used for this project. The birthday may be compared to one another
- * based on their relative dates, and they also may be accessed via their long
- * format.
+ * This class represents a Birthday object to be contained within the Red Black
+ * Tree used for this project. The birthday may be compared to one another based
+ * on their relative dates, and they also may be accessed via their long format.
  * 
- * Birthdays may be created with the following two format strings: 
- *  LONG_PATTERN:  "yyyy/MM/dd/HH/mm"
- *  SHORT_PATTERN: "yyyy/MM/dd"
+ * Birthdays may be created with the following two format strings: LONG_PATTERN:
+ * "yyyy/MM/dd/HH/mm" SHORT_PATTERN: "yyyy/MM/dd"
  * 
  * @author Patrick Harvey
  */
@@ -32,9 +30,19 @@ public class Birthday implements Comparable<Birthday> {
     private static final String STRING_LONG_PATTERN = "yyyy/MM/dd/HH/mm";
     private static final String STRING_SHORT_PATTERN = "yyyy/MM/dd";
 
+    // ([0][0-9])|([1][0-2]) for months
+    // ([012][0-9])|([3][0-1]) for days
     // the regex format patterns used for birthdays
-    private static final Pattern LONG_PATTERN = Pattern.compile("[0-9]{4}/[0-9]{2}/[0-9]{2}/[0-9]{2}/[0-9]{2}");
-    private static final Pattern SHORT_PATTERN = Pattern.compile("[0-9]{4}/[0-9]{2}/[0-9]{2}");
+    // private static final Pattern LONG_PATTERN =
+    // Pattern.compile("[0-9]{4}/[0-9]{2}/[0-9]{2}/[0-9]{2}/[0-9]{2}");
+    // private static final Pattern SHORT_PATTERN =
+    // Pattern.compile("[0-9]{4}/[0-9]{2}/[0-9]{2}");
+
+    // the regex format patterns used for birthdays
+    private static final Pattern LONG_PATTERN = Pattern
+            .compile("([0-9]{4})/([0][0-9]|[1][0-2])/([012][0-9]|[3][0-1])/([0-9]{2})/([0-9]{2})");
+    private static final Pattern SHORT_PATTERN = Pattern
+            .compile("([0-9]{4})/([0][0-9]|[1][0-2])/([012][0-9]|[3][0-1])");
 
     // patterns used by this birthday
     private SimpleDateFormat BIRTHDAY_FORMAT;
@@ -46,18 +54,19 @@ public class Birthday implements Comparable<Birthday> {
     private long birthdayLong;
 
     /**
-     * Constructor for birthday that initializes the first and last names,
-     * and the method also parses the string given for the birthday into the 
-     * following format: 
+     * Constructor for birthday that initializes the first and last names, and the
+     * method also parses the string given for the birthday into the following
+     * format:
      * 
-     * Long Format for birthday: "yyyy/MM/dd/HH/mm"
-     * Short Format for birthday: "yyyy/MM/dd"
+     * Long Format for birthday: "yyyy/MM/dd/HH/mm" Short Format for birthday:
+     * "yyyy/MM/dd"
      * 
      * @param birthdayDate - a string of the birthday in the above format
-     * @param firstName - the first name of the person's birthday
-     * @param lastName - the last name of the person's birthday
-     * @throws IllegalBirthdayFormatException if the birthday date is not in either of the two
-     * correct formats LONG or SHORT
+     * @param firstName    - the first name of the person's birthday
+     * @param lastName     - the last name of the person's birthday
+     * @throws IllegalBirthdayFormatException if the birthday date is not in either
+     *                                        of the two correct formats LONG or
+     *                                        SHORT
      */
     public Birthday(String stringDate, String firstName, String lastName) throws IllegalBirthdayFormatException {
 
@@ -66,8 +75,8 @@ public class Birthday implements Comparable<Birthday> {
 
         // check for null pointer in stringdate
         if (stringDate == null) {
-            throw new IllegalBirthdayFormatException("There was in error in reading the format: "
-            + " You cannot use null strings.");
+            throw new IllegalBirthdayFormatException(
+                    "There was in error in reading the format: " + " You cannot use null strings.");
         }
 
         try {
@@ -75,15 +84,15 @@ public class Birthday implements Comparable<Birthday> {
             String PATTERN = determineFormat(stringDate);
 
             // use the determined pattern for the birthday format
-            BIRTHDAY_FORMAT = new SimpleDateFormat(PATTERN); 
+            BIRTHDAY_FORMAT = new SimpleDateFormat(PATTERN);
 
             // sets the birthday date with the parsed Date format
-            this.birthdayDate = BIRTHDAY_FORMAT.parse(stringDate);    
+            this.birthdayDate = BIRTHDAY_FORMAT.parse(stringDate);
 
-        // catch incorrect formats for birthday and throw exception
-        } catch (PatternSyntaxException|ParseException e) {
-            throw new IllegalBirthdayFormatException("There was an error in reading the format" 
-            + " of this birthday date: " + stringDate + " " + firstName + " " + lastName);
+            // catch incorrect formats for birthday and throw exception
+        } catch (PatternSyntaxException | ParseException e) {
+            throw new IllegalBirthdayFormatException("There was an error in reading the format"
+                    + " of this birthday date: " + stringDate + " " + firstName + " " + lastName);
         }
 
         // convert string date into long format for getting/removing
@@ -91,12 +100,13 @@ public class Birthday implements Comparable<Birthday> {
     }
 
     /**
-     * Determines the format of the string based on the appropriate
-     * regular expressions LONG_PATTERN or SHORT_PATTERN.
+     * Determines the format of the string based on the appropriate regular
+     * expressions LONG_PATTERN or SHORT_PATTERN.
      * 
      * @param date the string date of the birthday
      * @return the string format of the given date for creating a date format
-     * @throws PatternSyntaxException if the given String date matches neither of the patterns
+     * @throws PatternSyntaxException if the given String date matches neither of
+     *                                the patterns
      */
     private String determineFormat(String date) throws PatternSyntaxException {
 
@@ -119,8 +129,8 @@ public class Birthday implements Comparable<Birthday> {
     }
 
     /**
-     * Converts the date of this birthday to its long format for use within
-     * the constructor method.
+     * Converts the date of this birthday to its long format for use within the
+     * constructor method.
      * 
      * @param birthday the string of this birthday
      * @return the long format of the birthday
@@ -140,16 +150,17 @@ public class Birthday implements Comparable<Birthday> {
     }
 
     /**
-     * A getter method for returning the long version
-     * of this birthday's date.
+     * A getter method for returning the long version of this birthday's date.
+     * 
      * @return the long format of this birthday's date
      */
     public Long getBirthdayLong() {
         return birthdayLong;
     }
-    
+
     /**
      * The first name of this person's birthday
+     * 
      * @return the string first name
      */
     public String getFirstName() {
@@ -158,6 +169,7 @@ public class Birthday implements Comparable<Birthday> {
 
     /**
      * The last name of this person's birthday
+     * 
      * @return the string last name
      */
     public String getLastName() {
@@ -167,9 +179,11 @@ public class Birthday implements Comparable<Birthday> {
     /**
      * The format of this Birthday string is as follows (excluding braces):
      * 
-     * "Firstname Lastname: [Weekday] [Month] [Day], [Year] @ [Hour]:[Minute] [AM/PM]"
+     * "Firstname Lastname: [Weekday] [Month] [Day], [Year] @ [Hour]:[Minute]
+     * [AM/PM]"
      * 
-     * @return the string representation of this birthday in the above format for printing.
+     * @return the string representation of this birthday in the above format for
+     *         printing.
      */
     public String toString() {
         // create format for printing
@@ -180,13 +194,13 @@ public class Birthday implements Comparable<Birthday> {
     }
 
     /**
-     * Compares two birthdays to one another by date and time. 
-     * The object is called this and the other argument object is called other.
-     *    - returns -1 if this occurs BEFORE other
-     *    - returns 1 if this occurs AFTER other
-     *    - returns 0 if this occurs at the same time as other
+     * Compares two birthdays to one another by date and time. The object is called
+     * this and the other argument object is called other. - returns -1 if this
+     * occurs BEFORE other - returns 1 if this occurs AFTER other - returns 0 if
+     * this occurs at the same time as other
      * 
-     * @return the -1 if this is before other, 1 if other occurs before this, 0 if dates are equal.
+     * @return the -1 if this is before other, 1 if other occurs before this, 0 if
+     *         dates are equal.
      */
     @Override
     public int compareTo(Birthday other) {
@@ -194,18 +208,17 @@ public class Birthday implements Comparable<Birthday> {
         // this birthday is before the arg birthday
         if (birthdayDate.before(other.birthdayDate)) {
             return -1;
-        } 
-        
+        }
+
         // this birthday is after the arg birthday
         else if (birthdayDate.after(other.birthdayDate)) {
             return 1;
-        } 
-        
+        }
+
         // the two birthdays are equal
         else {
             return 0;
         }
     }
-
 
 }
